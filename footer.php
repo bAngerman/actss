7873e47aff69b98c38ef736ac86545e3ca24aec7
Jessica <jessica.samuelson92@gmail.com>
2018-03-20 15:55:33 -0600

    Footer build

--- footer.php
+++ footer.php
@@ -13,8 +13,9 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="footer">
 
-	<div class="<?php echo esc_attr( $container ); ?>">
-		<div class="footer-logo"><img class="logo" src="http://actssa.web.dmitcapstone.ca/wp-content/uploads/2018/03/ACTSS-LOGO.svg" alt="ACTSS LOGO"/></div>
+	<div class="footer-container">
+	
+		<img class="logo" src="http://actssa.web.dmitcapstone.ca/wp-content/uploads/2018/03/ACTSS-LOGO.svg" alt="ACTSS LOGO"/>
 		<ul class="social">
 			<li>
 				<a href="#"><i class="fab fa-instagram"></i></a>
@@ -26,16 +27,19 @@ $container = get_theme_mod( 'understrap_container_type' );
 				<a href="#"><i class="fab fa-twitter-square"></i></a>
 			</li>
 		</ul>
-		<div class="address">
+		<div class="address footer-text">
 			<p>Animal Cancer Therapy Subsidization Society (ACTSS)</p>
 			<p>Box 68244 Bonnie Doon RPO Edmonton, AB T6C 4N6</p>
 		</div>
-		<div class="crn">
+		<div class="crn footer-text">
 			<p>Charitable Registration Number: 88330 0626 RR0001</p>
 		</div>
-		<div class="copyright">
+		<div class="copyright footer-text">
 			<p><i class="far fa-copyright"></i> ACTSS <?php echo date("Y"); ?></p>
-	</div><!-- container end -->
+		</div>
+		<div class="privacy footer-text"><a href="#">Privacy Policy and Terms of Use</a></div>
+		
+	</div><!-- footer container end -->
 
 </div><!-- footer end -->
 

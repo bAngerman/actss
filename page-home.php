73050850ae07d9c1a3541872636c841b75633313
Corinna <Corinna@MacBook-Pro-de-Corinna.local>
2018-03-20 16:01:25 -0600

    home page content additions
    static html

--- page-home.php
+++ page-home.php
@@ -15,6 +15,49 @@ $container = get_theme_mod( 'understrap_container_type' );
 </div>
 
 <!-- /Banner -->
+<?php
+/**
+ * Home Page Template for ACTSS
+ * 
+ */
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
 
+<!-- Banner -->
+<div class="banner">
+  <div class="<?php echo $container; ?>">
+
+  </div>
+</div>
+
+<!-- /Banner -->
+<main>
+    <div>
+        <img src="">
+    </div>
+    <div class="about-home">
+        <h2>About Us</h2>
+        <p>
+            ACTSS is a not-for-profit society dedicated to bringing affordable modern veterinary cancer treatment to pets. Through our programs we provide subsidies to dedicated pet owners. We aim to educate animal owners and veterinarians on the availability, uses and benefits of advanced cancer therapies in the treatment of veterinary cancers.
+        </p>
+        <a href="page-about-us.php">Read More</a>
+        <p>Your help gets us one step closer to helping families and their pets. Thanks for your generous support, we couldn’t do it without you.</p>
+        <a href="#">Donate Now!</a>
+        <img src="">
+    </div>
+    <div class="events-home">
+        <h2>Events</h2>
+        <!-- Insert a loop to a category of events -->
+    </div>
+    <div class="pets-home">
+        <h2>Our Pets</h2>
+        <div> 
+         <!--Loop for pets category -->
+        </div>
+    </div>
+</main>
+
+<?php get_footer(); ?>
 
 <?php get_footer(); ?>

44ae18f9887fce5cf934707768372c04905a2064
Jessica <jessica.samuelson92@gmail.com>
2018-04-03 17:26:23 -0600

    text columns about us

--- page-about-us.php
+++ page-about-us.php
@@ -16,7 +16,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <!-- /Banner -->
 <div class="aboutus-container">
-  <div class="miss-vis-goals">
+  <div class="miss-vis-goals clearfix">
   
       <div class="box colored-box-orange">
       <h2>Our Mission</h2>
@@ -32,35 +32,44 @@ $container = get_theme_mod( 'understrap_container_type' );
       
       <div class="box colored-box-blue">
       <h2>Our Goals</h2>
-        <p>To subsidize the cost of cancer therapies for pets.</p>
-        <p>To support the well being of pet families.</p>
-        <p>To continually expand ACTSS' network of association, thereby offering ever greater numbers of people and pets the opportunity to benefit from its program.</p>
-        <p>To provide education for veterinarians, animal health technologists and the pet owning public regarding the availability, uses and benefits of advanced cancer therapies.</p>
+      <ul>
+        <li>To subsidize the cost of cancer therapies for pets.</li>
+        <li>To support the well being of pet families.</li>
+        <li>To continually expand ACTSS' network of association, thereby offering ever greater numbers of people and pets the opportunity to benefit from its program.</li>
+        <li>To provide education for veterinarians, animal health technologists and the pet owning public regarding the availability, uses and benefits of advanced cancer therapies.</li>
+    </ul>
       </div><!-- close box -->
     
   </div><!--  close miss-vis-goals -->
   <div class="about-us clearfix">
     <h2>About Us</h2>
+    <div class="col">
     <p>The same question is asked many times to ACTSS volunteers working at booths during pet related information fairs: "What exactly does ACTSS do?" The huge smile on the face of the ACTSS volunteer indicates their tremendous pride in our society as they explain the many ways that ACTSS serves pets and their families in Alberta.</p>
-        <p>ACTSS began in 1997 at a time when pets with cancer had to travel to the United States to receive radiation therapy and other types of advanced oncology treatments.  Because this was expensive and difficult for many Albertans, the majority of these pets were euthanized. Pets are family members and the premature severing of the human/animal bond due to the expense and inaccessibility of cancer therapy was tragic. Veterinary professionals and concerned pet parents came together to help affected families, and ACTSS was born. 
-    </p>
-    <a href="#">Read More</a>
+    <p>ACTSS began in 1997 at a time when pets with cancer had to travel to the United States to receive radiation therapy and other types of advanced oncology treatments.  Because this was expensive and difficult for many Albertans, the majority of these pets were euthanized. Pets are family members and the premature severing of the human/animal bond due to the expense and inaccessibility of cancer therapy was tragic. Veterinary professionals and concerned pet parents came together to help affected families, and ACTSS was born. ACTSS amalgamated forces with the Western College of Veterinary Medicine (WCVM) to help build the first comprehensive Veterinary Oncology Centre in western Canada.</p>
+    <p>ACTSS then turned to two other important pet cancer needs; making cancer treatment affordable and increasing awareness of this deadly disease to Albertans so wise choices can be made, should their pets develop cancer. ACTSS began an ambitious fundraising program to help subsidize cancer therapies for Alberta pets when it is beyond the pet parents' financial means to pay for life saving treatments. To this date, ACTSS has helped over 300 families keep their beloved pets with them for one more Christmas, one more birthday, one more summer and frequently.....a lot more than that.</p>
+</div>
+    
   </div> <!-- about-us close -->
   <div class="history clearfix">
 
       <h2>History</h2>
-     
+      <div class="col">
         <p>In March of 1997, local Edmonton media released information about a little dog named Atrayo who was receiving radiation therapy for his cancer after hours at a human cancer centre. The provincial government reacted by permanently closing the doors of all human treatment facilities to pets. Without the completion of his radiation therapy, Atrayo died a few months later. Veterinarians and politicians heard an outcry from pet owners around the country. As a result, serious analysis of the access of pets to cancer treatment programs was made.</p>
-      <a href="#">Read More</a>
 
+        <p>Veterinarians in general practice can treat a large percentage of cancers, but many aggressive cancers need to be managed at specialized oncology centres. In 1997, no such facilities for pets existed in Western Canada. The cost of these procedures even when managed locally cost hundreds, sometimes thousands of dollars that many pet owners cannot afford. The expense and the access to therapy kept many animals from being treated for their cancers.Veterinarians, animal health technologists, pet owners and animal lovers came together and formed a group with the primary interest of developing a non-profit veterinary oncology facility in Alberta. This group became incorporated and was called the Veterinary Cancer Institute (VCI). Through dedicated work, research and with the assistance of cancer specialists, VCI teamed up with the Western College of Veterinary Medicine (WCVM) in Saskatoon, Saskatchewan to build Western Canada’s first oncology centre. The Western College of Veterinary Medicine hired a veterinary oncologist and built a facility that could run a complete oncology program, which would include radiation, chemotherapy, surgery, MRI and a critical care unit. The VCI donated a radiation machine to the facility.</p>
+
+    <p>On September 28, 2001, ACTSS directors Dr. Jennifer Stelfox, Dr. Sheri Dalton and Ms. Marlene Kerr broke ground for the new centre at the WCVM in Saskatoon Saskatchewan. This one-of-a-kind facility opened in the spring of 2002.In October of 2001, the VCI officially became the Animal Cancer Therapy Subsidization Society (ACTSS) to better reflect its current objectives. ACTSS concentrates on making cancer therapy affordable for pet owners through a memorial fund that was established in honour of a wonderful dog named Lucky Moffat, who developed a malignant cancer at a young age, as well as through a variety of fundraising activities.
+    The Lucky Moffat Memorial Fund is used to subsidize Alberta pets whose owners are unable to pay for extensive cancer treatment. All Alberta Veterinary Clinics offer information that explains how to apply for subsidization, including forms for pet owners to complete. Current forms are available on this website by clicking on Apply for Subsidy.</p>
+</div>
   </div> <!-- about-us history -->
   <div class="chet clearfix">
 
       <h2>The Chet Dobson Story</h2>
-      
-        <p>In July 2007 Joe and Stephanie Dobson were devastated to learn that their beloved 12 year old Lab-Husky cross Chet had a malignant tumour in his lungs. Chet had begun to cough up blood and the situation was critical. Two Calgary, Alberta specialist centres worked together to save Chet’s life. The Western Veterinary Specialist Centre performed the difficult surgery that removed the dangerous tumour from Chet’s lungs and the Calgary Animal Referral and Emergency Centre later gave Chet chemotherapy. The financial cost was high but the Dobson’s knew that Chet was worth it.</p>  
-        <a href="#">Read More</a>
-  
+      <div class="col">
+        <p>In July 2007 Joe and Stephanie Dobson were devastated to learn that their beloved 12 year old Lab-Husky cross Chet had a malignant tumour in his lungs. Chet had begun to cough up blood and the situation was critical. Two Calgary, Alberta specialist centres worked together to save Chet’s life. The Western Veterinary Specialist Centre performed the difficult surgery that removed the dangerous tumour from Chet’s lungs and the Calgary Animal Referral and Emergency Centre later gave Chet chemotherapy. The financial cost was high but the Dobson’s knew that Chet was worth it. The time spent working with the specialists made Joe wonder about the people who could not afford the costs of having expensive medical treatments done to save their pets lives, and the devastation they must feel losing the pet they love. Could something be done to help these people and their pets? Joe decided to do something to highlight this important issue.</p>  
+        <p>He registered for his first marathon. Less than two months before the Detroit Marathon Joe began to train. Joe ran the event, collecting prizes and raised over $5000 in pledges for ACTSS. Global Television produced a moving documentary about Joe, Chet and their cause. The story went national and donations rolled in from three different countries. Shortly after his marathon (once he could walk again) Joe held a prize draw for everyone who donated pledges to the Chet Dobson Marathon.</p>
+        <p>Chet recovered nicely from his surgery and underwent weeks of chemotherapy. The chemotherapy caused Chet and Joe to have good and bad days, but Chet met every challenge with enthusiasm and grace and was a favorite patient of the veterinary staff. Soon, Chet was done his chemotherapy, and got on with the business of being a dog who loved life. His next two years were filled with good food and walks, and of course belly rubs and the pets from favorite family members and friends.On October 16, 2009 at the incredible age of 14 ½ years, Chet passed away surrounded by those he loved and who loved him.</p>
+</div>
   </div> <!-- about-us history -->
 
 </div><!--  close container -->

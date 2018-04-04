9e35f26fd8501c88f90576f38e426dab6022aa5e
Corinna <Corinna@MacBook-Pro-de-Corinna.local>
2018-04-04 10:47:44 -0600

    add subsidy page

---
+++ page-subsidy.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Subsidy Page Template for ACTSS
+ * 
+ */
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<!-- Banner -->
+<div class="banner">
+  <div class="<?php echo $container; ?>">
+
+  </div>
+</div>
+
+<!-- /Banner -->
+<main>
+    <div class="contain-points-1">
+        <div class="points">
+            <div class="point-1">
+                <img src="/wp-content/themes/actss/img/goal-1.png" alt="point one">
+                <p>How are you prepared to cover maintenance costs and home nursing care after the oncology treatment?</p>
+            </div><!-- close point 1 -->
+        </div><!-- close points -->
+    </div> <!-- close contain points 1 -->
+    <div class="contain-points-2">
+        <div class="points">
+            <div class="point-2">
+                <img src="/wp-content/themes/actss/img/goal-2.png" alt="point two">
+                <p>Are you prepared to cover costs above what ACTSS will sponsor? How much are you willing to pay?</p>
+            </div><!-- close point 2 -->
+        </div><!-- close points -->
+    </div> <!-- close contain points 2 -->
+    <div class="contain-points-3">
+        <div class="points">
+            <div class="point-3">
+                <img src="/wp-content/themes/actss/img/goal-3.png" alt="point three">
+                <p>If necessary, are you and your pet prepared to travel to receive oncology therapy?</p>
+            </div><!-- close point 3 -->
+        </div><!-- close points -->
+    </div> <!-- close contain points 3 -->
+    <div class="contain-points-4">
+        <div class="points">
+            <div class="point-4">
+                <img src="/wp-content/themes/actss/img/goal-4.png" alt="point four">
+                <p>Are you prepared to cover travel expenses? ACTSS will not subsidize travel expenses.</p>
+            </div><!-- close point 4 -->
+        </div><!-- close points -->
+    </div> <!-- close contain points 4 -->
+    <div class="contain-points-5">
+        <div class="points">
+            <div class="point-5">
+                <img src="/wp-content/themes/actss/img/goal-5.png" alt="point five">
+                <p>Are you aware of the therapies required to treat your pet's illness? Are you aware of possible side effects and duration of treatment? Are you aware of the prognosis of the disease? Will the above information affect your willingness to seek treatment for your pet?</p>
+            </div><!-- close point 5 -->
+        </div><!-- close points -->
+    </div> <!-- close contain points 5 -->
+    <div class="contain-apply-info">
+        <div class="apply-info">
+            <h2>Apply for Subsidization</h2>
+            <div class="col">
+                <p></p>
+            </div><!-- close col -->
+        </div><!-- close apply info -->
+    </div><!-- close contain aplly info -->
+
+
+
+</main>
+<?php
+get_footer();
+?>

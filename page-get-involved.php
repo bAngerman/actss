28e30d1ed419476932401ca5703e543ab2d4ad34
Jessica <jessica.samuelson92@gmail.com>
2018-04-08 14:32:58 -0600

    Get Involved Page styles

--- page-get-involved.php
+++ page-get-involved.php
@@ -22,29 +22,29 @@ $container = get_theme_mod( 'understrap_container_type' );
             <p>Looking for a way to get involved with ACTSS Alberta? From sideline supporters who donate to our organization, sponsor our events or become a member to hands on work for those who enjoy volunteering, there is a place for everyone at ACTSS. We would love to have your support in anyway you choose. Your involvement helps change the lives of so many beloved family pets right here in Alberta! Check out the different options below on how you can get involved and help continue the heartfelt work we do here at ACTSS. </p>
        </div>
        <div class="boxes">
-            <div class="box colored-box-orange">
-                <div class="contain-member-box">
+            <div class="box colored-box-orange clearfix">
+                <div class="contain-box-left clearfix">
                     <h2>Memberships</h2>
                     <p>Memberships are a great way to get involved with the inner workings of ACTSS. Members not only stay up to date with the current events and information, but are also invited to be a part of the vital service of subsidization review for pet families. By becoming a member you’re helping us at the very core of our organization.</p>
                     <a href="#" class="btn-transition btn-styles">Read More</a>
                 </div><!-- close contain-member-box -->
             </div><!-- box close -->
-            <div class="box colored-box-yellow">
-                <div class="contain-donate-box">
+            <div class="box colored-box-yellow clearfix">
+                <div class="contain-box-right clearfix">
                     <h2>Donate</h2>
                     <p>Our organization runs on donations from people like you. Donations to ACTSS go straight to the heart of our organization. It helps us in our day to day running and helps fund the subsidization families rely on to treat their beloved pets.</p>
                     <a href="#" class="btn-transition btn-styles">Read More</a>
                 </div><!-- close contain-donate-box -->
             </div><!-- box close -->
-            <div class="box colored-box-blue">
-                <div class="contain-volunteer-box">
+            <div class="box colored-box-blue clearfix">
+                <div class="contain-box-left clearfix">
                     <h2>Volunteer</h2>
                     <p>Want to get more involved with ACTSS? Volunteering may be the place for you. Our organization relies solely on the work of our dedicated volunteers we’re lucky enough to have. From events to day to day operations, volunteers are the solid platform on which ACTSS runs. We are always looking for community minded volunteers who want to help make a difference.  </p>
                     <a href="#" class="btn-transition btn-styles">Read More</a>
                 </div><!-- close contain-volunteer-box -->
             </div><!-- box close -->
-            <div class="box colored-box-blue">
-                <div class="contain-volunteer-box">
+            <div class="box colored-box-orange clearfix">
+                <div class="contain-box-right clearfix">
                     <h2>Sponsor</h2>
                     <p>Sponsorship is a noble way to get involved with ACTSS. Sponsors help us put on great fundraisers and help our organization with our day to day running. We couldn’t do the things we do without our sponsors, and we’re so grateful for them. Check out our past sponsors, or join the ranks. We’d love to have you on board. </p>
                     <a href="#" class="btn-transition btn-styles">Read More</a>

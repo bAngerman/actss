d9a175827a5561d86c87ad040475372a4a2bf1f4
Jessica <jessica.samuelson92@gmail.com>
2018-04-08 15:58:27 -0600

    Mobile Resources Page

--- page-resources.php
+++ page-resources.php
@@ -38,9 +38,9 @@ $container = get_theme_mod( 'understrap_container_type' );
                         <h4>Calgary, AB</h4>
                         <p> Drs. Glenna and Neal Mauldin operate Alberta’s only oncology centre that provides radiation therapy for pets. The Mauldins supply both radiation and medical oncology diagnostics and treatment programs while oncology surgeon Dr. Dave Szentimrey performs specialized cancer surgeries.</p>
                         <div class="doctor-info">
-                            <p>Dr. Neal Mauldin</p>
-                            <p>(403) 770-1340</p>
-                            <p> oncology@westernvet.ca</p>
+                            <p>Dr. Neal Mauldin<br>
+                            (403) 770-1340<br>
+                            oncology@westernvet.ca</p>
                             <a href="#">Visit their site!</a>
                         </div>
                     </div>
@@ -49,9 +49,9 @@ $container = get_theme_mod( 'understrap_container_type' );
                         <h4>Calgary, AB</h4>
                         <p> The Calgary Animal Refferal and Emergy Centre (CARE) based out of Calgary, AB has board certified medical oncologist Dr. Victoria Larson who  is heading up its oncology centre. They provide veterinary oncology care for Alberta pets.</p>
                         <div class="doctor-info">
-                            <p> Dr. Larson</p>
-                            <p>(403) 520-8387</p>
-                            <p>info@carecentre.ca</p>
+                            <p> Dr. Larson<br>
+                            (403) 520-8387<br>
+                            info@carecentre.ca</p>
                             <a href="#">Visit their site!</a>
                         </div>
                     </div>
@@ -60,15 +60,23 @@ $container = get_theme_mod( 'understrap_container_type' );
                         <h4>Saskatoon, SK</h4>
                         <p>With the assistance of ACTSS, WCVM developed western Canada’s first veterinary oncology center in 2003 when its veterinary oncologists Dr. Monique Mayers and Dr. Valerie MacDonald opened the doors to the new radiation vault and medical oncology treatment centre.</p>
                         <div class="doctor-info">
-                            <p>The Western College of Veterinary Medicine</p>
-                            <p>(306) 966-7103</p>
+                            <p>The Western College of Veterinary Medicine<br>
+                            (306) 966-7103</p>
                             <a href="#">Visit their site!</a>
                         </div>
                     </div>
                 </div>  <!-- close oncology boxes -->
             </div> <!-- close oncology -->
         </div><!-- contain oncology close -->
-    </div> <!-- close resources -->
+    <div class="contain-support">
+        <div class="support clearfix">
+
+        <h2>Support</h2>
+        
+            <p>Losing a pet, or dealing with a recent diagnosis can be tough, but there are resources out there to help. Remember a Pet is a non-profit society that has resources and ways to memorialize your pet. </p>
+        </div> <!-- close support -->
+    </div><!-- close contain support -->
+</div> <!-- close resources -->
 </main>
 
 <?php get_footer(); ?>

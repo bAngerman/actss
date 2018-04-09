df1c55ffd652353b05dc96275a64f7b7c09bbcd6
Jessica <jessica.samuelson92@gmail.com>
2018-04-09 10:15:52 -0600

    Added Privacy Policy Page

---
+++ page-privacy-policy.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * About Us Page Template for ACTSS
+ * 
+ */
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<!-- Banner -->
+<div class="banner">
+  <div class="<?php echo $container; ?>">
+
+  </div>
+</div>
+
+<!-- /Banner -->
+
+  <div class="contain-privacy">
+    <div class="privacy clearfix">
+      
+      <div class="col">
+        <h4>Introduction</h4>
+        <p>The Animal Cancer Therapy Subsidization Society is an organization providing continuing education to pet owners and Veterinarians within the Province of Alberta. In addition, as a registered non-profit charity, ACTSS has the responsibility on behalf of Canada's anti-spam legislation, to communicate with respect.
+        ACTSS considers privacy to be a very important matter and is committed to protecting the privacy of the Personal Information of those to whom it communicates. The LESA Privacy Policy incorporates the applicable portions of the Personal Information Protection and Electronic Documents Act (Canada), the Personal Information Protection Act (Alberta) and the ten principles set out in the Canadian Standards Association (CSA) Model Code for the Protection of Personal Information.</p>
+        <h4>Personal Information</h4>
+        <p>"Personal Information" means information about an identifiable person, but does not include information that is created by aggregating information about an identifiable person which does not identify or cannot be associated with a specific person. "Personal Information" does not include a person’s name, position name or title, business telephone number, business address, business e-mail, business fax numbers and other similar business contact information.</p>
+        <h4>Collection of Information</h4>
+        <p>Where practical, we will try to collect Personal Information directly from the person to whom the information pertains.
+        The legislation provides that an individual is deemed to consent to the collection, use or disclosure of Personal Information about that individual for a particular purpose if the individual voluntarily provides the information for that purpose, and it is reasonable that a person would voluntarily provide that information.When we collect, use or disclose personal information, we will make reasonable efforts to ensure that it is accurate and complete.
+        In determining the appropriate form of consent, we will take into account the sensitivity of the Personal Information and the reasonable expectations of persons. A person may withdraw his or her consent at any time.</p>
+        <h4>Use Of Information</h4>
+        <p>We collect Personal Information for the following purposes (the “Identified Purposes”):
+        to establish and maintain relationships with persons applying for subsidization or volunteering;
+        to provide requested and ongoing services;
+        to develop, enhance and/or market our public relations events
+        to manage and improve our organization and operations and;
+        to meet legal and regulatory requirements.</p>
+        <h4>Disclosure</h4>
+        <p>We will not use or disclose Personal Information for purposes other than that for which it was collected, except with the consent of the person.
+        At any time, should you wish to no longer receive correspondence or publications from us, please send an email request to unsubscribe@actssalberta.ca.</p>
+        <h4>Protection</h4>
+        <p>We will protect Personal Information by using reasonable safeguards appropriate to the sensitivity of the information and will make reasonable efforts to protect Personal Information against loss or theft, as well as unauthorized access, disclosure, copying, use or modification regardless of the format in which it is held. The legislation also allows us, for legal or business purposes, to retain personal information for as long as is reasonable.</p>
+      </div>
+    </div> <!-- privacy close -->
+  </div><!-- contain privacy -->
+
+
+
+<?php get_footer(); ?>

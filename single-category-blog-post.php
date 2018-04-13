1642574eb2489f5c3ed3b582c5da806016f746b4
Jessica <jessica.samuelson92@gmail.com>
2018-04-13 15:14:40 -0600

    blog & events.

--- single-category-blog-post.php
+++ single-category-blog-post.php
@@ -28,7 +28,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <main>
 	<div class="contain-single-blog">
-		<div class="single-blog">
+		<div class="single-blog clearfix">
+			
 		<?php 
 
 			if (has_post_thumbnail()) {
@@ -37,15 +38,61 @@ $container = get_theme_mod( 'understrap_container_type' );
 		?>
 			<p><span>Blog</span></p>
 			<h2><?php the_title(); ?></h2>
-			<p><?php the_date(); ?></p>
-			<div class="single-content">
-				<p><?php the_field('blog_content'); ?></p>
-			</div>
-			<div class="sidebar">
+			<p class="date"><?php the_date(); ?></p>
+			<div class="single-main">
+				<div class="single-content">
+					<p><?php the_field('blog_content'); ?></p>
+				</div>
+				<div class="sidebar">
+					<h3>Latest Blog Posts</h3>
+				<?php  
+					$currentPostID= get_the_id();
+					
+					$args= array(
+							'category_name' => 'blog-post',
+							'post__not_in' =>	array($currentPostID),
+							'posts_per_page' => 3,
+							'orderby'			=> 'DESC',
+							'posts_type'		=> 'post',
+							'post_status'		=> 'publish'
+							
+							
+						
+					);
 
-			</div>
-		</div>
-	</div>
+						$myposts = get_posts($args);
+
+						echo '<ul class="blog-archive-posts">';
+						foreach ($myposts as $post) : setup_postdata($post);
+
+						?>
+
+							<li class="clearfix">
+									<?php 
+
+										if (has_post_thumbnail()) {
+											the_post_thumbnail('full', array('class' => "blog-pic"));
+										}
+									?>
+									<div class="blog-content clearfix">
+										<a href="<?php the_permalink(); ?>" class="blog-title-home">
+											<h4><?php the_title(); ?></h4>
+										</a>
+										<p class="date date-blog-home"><?php the_date(); ?></p>
+										
+									</div>
+							</li>
+
+						<?php 
+							endforeach;
+							echo '</ul>';
+						
+						?>
+
+				</div><!-- sidebar close -->
+			</div><!-- single main close -->
+		</div><!-- single blog close -->
+	</div><!-- singlecontain close -->
 </main>
 
 <?php get_footer(); ?>
--- single-category-event.php
+++ single-category-event.php
@@ -27,7 +27,72 @@ $container = get_theme_mod( 'understrap_container_type' );
 <!-- /Banner -->
 
 <main>
-<h1> SINGLE EVENT PAGE</h1>
+	<div class="contain-single-blog">
+		<div class="single-blog clearfix">
+			
+		<?php 
+
+			if (has_post_thumbnail()) {
+				the_post_thumbnail('full', array('class' => "blog-pic"));
+			}
+		?>
+			<p><span>Events</span></p>
+			<h2><?php the_title(); ?></h2>
+			<p class="date"><?php the_date(); ?></p>
+			<div class="single-main">
+				<div class="single-content">
+					<p><?php the_field('details'); ?></p>
+				</div>
+				<div class="sidebar">
+					<h3>Latest Events</h3>
+				<?php  
+					$currentPostID= get_the_id();
+					
+					$args= array(
+							'category_name' => 'event',
+							'post__not_in' =>	array($currentPostID),
+							'posts_per_page' => 3,
+							'orderby'			=> 'DESC',
+							'posts_type'		=> 'post',
+							'post_status'		=> 'publish'
+							
+							
+						
+					);
+
+						$myposts = get_posts($args);
+
+						echo '<ul class="blog-archive-posts">';
+						foreach ($myposts as $post) : setup_postdata($post);
+
+						?>
+
+							<li class="clearfix">
+									<?php 
+
+										if (has_post_thumbnail()) {
+											the_post_thumbnail('full', array('class' => "blog-pic"));
+										}
+									?>
+									<div class="blog-content clearfix">
+										<a href="<?php the_permalink(); ?>" class="blog-title-home">
+											<h4><?php the_title(); ?></h4>
+										</a>
+										<p class="date date-blog-home"><?php the_date(); ?></p>
+										
+									</div>
+							</li>
+
+						<?php 
+							endforeach;
+							echo '</ul>';
+						
+						?>
+
+				</div><!-- sidebar close -->
+			</div><!-- single main close -->
+		</div><!-- single blog close -->
+	</div><!-- singlecontain close -->
 </main>
 
 <?php get_footer(); ?>

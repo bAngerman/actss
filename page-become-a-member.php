e7967855f54e3f2fc674cdd5096c1b1f88ba9b35
Corinna <Corinna@MacBook-Pro-de-Corinna.local>
2018-04-17 15:18:22 -0600

    new pages

--- page-become-a-member.php
+++ page-become-a-member.php
@@ -9,10 +9,24 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <!-- Banner -->
 <div class="banner">
-  <div class="<?php echo $container; ?>">
-    <?php echo do_shortcode('[swpm_payment_button id=156]'); ?>
-  </div>
 </div>
+<main>
+    <div class="contain-memberships">
+        <div class="memberships">
+            <h2>Memberships</h2>
+            <p>Looking to get more involved in a more dynamic way with ACTSS? Getting a membership might be the best option for you! Members play a key role in ACTSS, not only by providing great moral support but also by helping to  determine the future of those applying for subsidization. This is vital to our organization, and would love to have you join us! </p>
+            <a href="">Get More Info</a>
+            
+              <?php echo do_shortcode('[swpm_payment_button id=309]'); ?>
+
+        </div>
+        <div class="contain-membership-faq">
+            <div class="membership-faq">
+                <?php echo do_shortcode("[accordions_pickplguins id='559']"); ?>
+            </div>
+        </div>
+    </div>
+</main>
 
 
 <?php get_footer(); ?>
---
+++ page-donate.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Donate Template for ACTSS
+ * 
+ */
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+<!-- Banner -->
+<div class="banner">
+</div>
+<main>
+    <div class="contain-donate">
+        <div class="donate">
+            <h2>Donate</h2>
+            <p>Our organization runs on donations from people like you. Donations to ACTSS go straight to the heart of our organization. It helps us in our day to day running and helps fund the subsidization families rely on to treat their beloved pets.</p>
+
+            <a href="">Donate Now!</a>
+        </div>
+        <div class="contain-donate-photos">
+            <div class="donate-photos">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+            </div>
+        </div>
+    </div>
+</main>
+<?php
+get_footer();
+?>

f3f7b3304744b25479a7864f05c4971eeaee29a8
bAngerman <Brendan.angerman@gmail.com>
2018-04-23 16:30:08 -0600

    header inline ICK

--- header.php
+++ header.php
@@ -20,6 +20,40 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<style>
+	.banner {
+		<?php 
+			if ( is_front_page() ) {
+				echo 'background-image: url(' . get_site_url() . '/wp-content/uploads/2018/03/header-992px.png)';
+			} else {
+				echo 'background-image: url(' . get_site_url() . '/wp-content/uploads/2018/04/secondary-pages-header-mobile.jpg)';
+			}
+		?>
+	}
+	@media only screen and (min-width: 600px) {
+		.banner {
+			<?php 
+				if ( is_front_page() ) {
+					echo 'background-image: url(' . get_site_url() . '/wp-content/uploads/2018/03/header-992px.png)';
+				} else {
+					echo 'background-image: url(' . get_site_url() . '/wp-content/uploads/2018/04/secondary-pages-header-tablet.jpg)';
+				}
+			?>
+		}
+	}
+	@media only screen and (min-width: 992px) {
+		.banner {
+			<?php 
+				if ( is_front_page() ) {
+					echo 'background-image: url(' . get_site_url() . '/wp-content/uploads/2018/03/header-image-1600px.png)';
+				} else {
+					echo 'background-image: url(' . get_site_url() . '/wp-content/uploads/2018/04/secondary-pages-header-desktop.jpg)';
+				}
+			?>
+		}
+	}
+	
+	</style>
 	<?php wp_head(); ?>
 </head>
 
@@ -34,7 +68,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 			<a href="<?php echo get_site_url(); ?>">
 				<img class="logo" src="<?php echo get_site_url(); ?>/wp-content/uploads/2018/03/ACTSS-LOGO.svg" alt="ACTSS LOGO"/>
 			</a>
-			<div class="brand-tagline"><?php echo $pagename; ?></div>
+			<div class="brand-tagline"><?php echo get_query_var('pagename'); ?></div>
 		</div>
 		<div class="mobile-nav-btn">
 			<a href="#" id="toggleNav"><i class="fas fa-bars fa-2x"></i><i class="hidden fas fa-times fa-2x"></i></a>
@@ -54,7 +88,25 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<div class="banner">
 		<div class="<?php echo $container; ?>">
 			<div class="banner-text rocksalt">
-				<h4><?php echo $pagename; ?></h4>
+				<h1>
+				<?php 
+				if( is_front_page() ) {
+					echo "Helping pet families <span class=\"yellow\">live</span> with cancer"; 
+				} 
+				else if ( is_category('blog-post') ) {
+					echo "Blog";
+				}
+				else if ( is_category('pets') ) {
+					echo "Our Pets";
+				}
+				else if ( is_category('event') ) {
+					echo "Events";
+				}
+				else {
+					the_title();
+				}
+				?>
+				</h1>
 			</div>
 		</div>
 	</div>
